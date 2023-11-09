4dbd83f607de0335fb1a73c8dcf87f68ead92515
404 <schy2504@naver.com>
2023-11-09 12:03:30 +0900

    connection bewteen fornt and back

--- src/main/java/com/ssafy/enjoytrip/common/config/WebSecurityConfig.java
+++ src/main/java/com/ssafy/enjoytrip/common/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.ssafy.enjoytrip.common.security;
+package com.ssafy.enjoytrip.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
