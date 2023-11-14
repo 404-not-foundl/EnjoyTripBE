249c4d0ecaf98dab60f5719d0edd2a23df3186a3
404 <schy2504@naver.com>
2023-11-14 15:27:00 +0900

    WebMvcConfig allow cookie

--- src/main/java/com/ssafy/enjoytrip/WebMvcConfig.java
+++ src/main/java/com/ssafy/enjoytrip/WebMvcConfig.java
@@ -13,6 +13,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
-                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
+                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE")
+                .allowCredentials(false);
     }
 }
